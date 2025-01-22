cce43f1d0bbf8f10dace0178b423734cedb09c83
ShinoKThankachan <shinothankachan17@gmail.com>
2025-01-22 11:28:51 +0530

    files added

---
+++ Javascript/index.js
@@ -0,0 +1,17 @@
+// a=10
+// b='10'
+// c=a+b
+// console.log(c)
+// document.write(a ,"<br>")
+// document.write(b, "<br>")
+// document.write(c)
+
+// Ternary operator
+
+// b=parseInt(prompt('Enter a number:'));
+// c=parseInt(prompt('Enter a number:'));
+// (b>c)?console.log(`${b} is greater than ${c}`):console.log(`${c} is greater than ${b}`)
+
+
+b=parseInt(prompt('Enter a number:'));
+(b%2==0)?console.log(`${b} is even`):console.log(`${b} is odd`)
