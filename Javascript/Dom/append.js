dc2fa46e16fd6f8d6c7926d3851c98e9d9be7daf
ShinoKThankachan <shinothankachan17@gmail.com>
2025-01-28 11:32:53 +0530

    files added

---
+++ Javascript/Dom/append.js
@@ -0,0 +1,10 @@
+// let newelement=document.createElement('div')
+// newelement.classList.add('container')
+// newelement.innerHTML='<p>Hello</p>'
+// document.body.appendChild(newelement)
+
+function fun(){
+    element=document.getElementById('text')
+    element.innerHTML='<p></p>'
+    document.main.appendChild(element)
+}
---
+++ Javascript/Dom/queryselector.js
@@ -0,0 +1,10 @@
+// let para=document.querySelectorAll('.pr')
+// console.log(para)
+// for(i of para){
+//     console.log(i.textContent)
+// }
+
+let element=document.getElementById('pr')
+console.log(element.innerHTML)
+element.innerHTML='Goodmorning'
+element.style.backgroundColor='red'
