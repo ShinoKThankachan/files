4e2f8f1bd12e21e670dff17949bc8bed51ba8e0a
ShinoKThankachan <shinothankachan17@gmail.com>
2025-02-24 11:25:16 +0530

    files added

---
+++ Javascript/asynawait/async.js
@@ -0,0 +1,36 @@
+// async function Post() {
+//     let responses=await fetch('https://dummyjson.com/products')
+//     let data=await responses.json()
+//     console.log(data)
+//     console.log('Hello')
+// }
+
+// Post()
+
+
+async function fetchData(){
+    try{
+        let responses = await fetch('https://dummyjson.com/products')
+        if (!responses.ok){
+            throw new Error('fetch unsuccessful')
+        }
+        let data = await responses.json()
+        console.log(data)
+        data.products.forEach(value=>{
+            let newdiv = document.createElement('div')
+            newdiv.className='product lg-4'
+            newdiv.innerHTML= 
+                `<h3>${value.title}</h3>
+                <p>${value.description}</p>
+                <p>Price: ${value.price}</p>
+                <img src=${value.thumbnail}>`
+            document.body.appendChild(newdiv)
+        })
+    }
+    catch(error){
+        console.log("Error happened:", error.message)
+    }
+}
+
+fetchData()
+
---
+++ Javascript/fetch/fetch.js
@@ -0,0 +1,10 @@
+function Post(){
+    fetch('https://dummyjson.com/posts')
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+    .catch(error=>console.log(error))
+    console.log('hello')
+
+}
+
+Post()
