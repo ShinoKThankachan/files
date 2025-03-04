bacafc2d7fe32f0d7fae208c0225737b4a849135
ShinoKThankachan <shinothankachan17@gmail.com>
2025-03-04 13:26:10 +0530

    files added

---
+++ PureComp/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+class App extends React.PureComponent{
+  constructor(){
+    super()
+    this.state={
+      name:'Manu'
+    }
+  }
+
+  handleChange=()=>{
+  this.setState({
+    name:'Abhi'
+  })
+  }
+  render(){
+  console.log('component Rendered')
+  return(
+    <>
+      <p>name is {this.state.name}</p>
+      <button onClick={this.handleChange}>Change Name</button>
+    </>
+  )
+  }
+}
+export default App
