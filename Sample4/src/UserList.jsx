c3e890fd1f6e53158182ba86e707ebf737e7202a
ShinoKThankachan <shinothankachan17@gmail.com>
2025-03-06 08:56:28 +0530

    files added

---
+++ Sample4/src/UserList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class UserList extends React.Component{
+    render(){
+        return (
+            <div className="main">
+                {this.props.users.length>0?(
+                    <ul>
+                        {this.props.users.map((user)=>(
+                            <li key={user.id}>
+                                <p id="p1">.</p><br /><br />
+                                <p><strong>Name:{user.name}</strong></p>
+                                <p>Email:{user.email}</p>
+                                <p>Address:{user.Address}</p>
+                            </li>
+                            
+                        ))}
+                    </ul>
+                ):(
+                    <p>Loading users.....</p>
+                    
+                )}
+            </div>
+        )
+    }
+}
+
+export default UserList
